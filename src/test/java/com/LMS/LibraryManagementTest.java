b62d67a7bd3847777c1eb2412f039e21169381d0
Asthaviradiya <asthaviradiya114@gmail.com>
2024-09-01 12:53:25 +0530

    LibraryManagementTest.java : testAddBook method -> testcases added

--- src/test/java/com/LMS/LibraryManagementTest.java
+++ src/test/java/com/LMS/LibraryManagementTest.java
@@ -1,8 +1,34 @@
 package com.LMS;
 
+import java.util.List;
+
 public class LibraryManagementTest
-
 {
+    private Library library;
 
+    // Initializes a new Library object before each test with the help of @BeforeEach annotation of JUnit 5
+    @BeforeEach
+    void setUp(){
+        library = new Library();
+    }
 
+    // This test ensures that a new book can be added successfully
+    @Test
+    void testAddBook(){
+        Book book = new Book("27102003","ABCD","Author1",2008);
+        library.addBook(book);
+
+        /*Test Case 1 : False expectation
+        Assertions.assertEquals(0,library.getAvailableBooks().size());
+
+        It gives an error because after adding a book actual result is 1
+        But we gave false value as 0 so our function works correct
+         */
+
+        // Test Case 2 : True expectation
+        Assertions.assertEquals(1,library.getAvailableBooks().size());
+        Assertions.assertEquals(book,library.getAvailableBooks().get(0));
+        System.out.println("Test case need to run successfully");
+        // It runs correctly as we gave 1 as expected result and its equal to actual result
+    }
 }
