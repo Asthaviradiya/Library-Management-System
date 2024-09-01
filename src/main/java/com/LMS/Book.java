e23268a9ec2b30a5d80552719dfb23d62cd24c4b
Asthaviradiya <asthaviradiya114@gmail.com>
2024-09-01 11:17:55 +0530

    constructor for Book class added

--- src/main/java/com/LMS/Book.java
+++ src/main/java/com/LMS/Book.java
@@ -1,4 +1,18 @@
 package com.LMS;
 
 public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean isAvailable;
+
+    public Book(String isbn,String title,String author,int publicationYear){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true;
+    }
+
 }
